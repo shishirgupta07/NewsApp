66da39879cf0b514a95918f784b675c1c3fd3c49
Shishir Gupta <shishirgupta071998@gmail.com>
2023-11-27 12:19:56 +0530

    initial commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import PropTypes from 'prop-types'
+import React, { Component } from 'react'
+
+export class Navbar extends Component {
+  static propTypes = {}
+
+  render() {
+    return (
+      <div>Navbar</div>
+    )
+  }
+}
+
+export default Navbar
